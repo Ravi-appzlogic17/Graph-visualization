79a47a5d0d133352d76e273d73000fc74f93b92a
Ravi vishnoliya <adibconsultant@thriwe.com>
2025-03-03 15:19:10 +0530

    graph visualization represent

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import GraphComponent from "./GraphComponent";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Network Graph</h1>
+      <li>
+        Click on the DB Server to connect to DB Table
+      </li>
+      <Router>
+      <GraphComponent />
+    </Router>
     </div>
   );
 }
